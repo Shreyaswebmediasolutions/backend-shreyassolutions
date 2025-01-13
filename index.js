e3619949bd96188e432478f4216bb43fe3176b1c
Shreyaswebmediasolutions <tech@shreyaswebmediasolutions.com>
2025-01-13 11:27:46 +0530

    checkingdb

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
 const db = require("./utils/db.js");
-const authRoute = require("./router/authRoute.js");
+// const authRoute = require("./router/authRoute.js");
 require("dotenv").config();
 
 const app = express();
@@ -27,7 +27,7 @@ if (db) {
 }
 
 // Routes
-app.use("/api/auth", authRoute); // Authentication routes
+// app.use("/api/auth", authRoute); // Authentication routes
 
 app.get("/server", (req, res) => {
   res.status(200).json({ message: "Server is running" });
--- utils/db.js
+++ utils/db.js
@@ -1,26 +1,26 @@
-const mysql = require("mysql");
+// const mysql = require("mysql");
 
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// });
 
-if (
-  !process.env.DB_HOST ||
-  !process.env.DB_USER ||
-  !process.env.DB_PASSWORD ||
-  !process.env.DB_NAME
-) {
-  throw new Error("Missing required database environment variables.");
-}
+// if (
+//   !process.env.DB_HOST ||
+//   !process.env.DB_USER ||
+//   !process.env.DB_PASSWORD ||
+//   !process.env.DB_NAME
+// ) {
+//   throw new Error("Missing required database environment variables.");
+// }
 
-db.connect((err) => {
-  if (err) {
-    throw new Error("Error connecting to database:", err.message);
-  }
-  console.log("Connected to the database");
-});
+// db.connect((err) => {
+//   if (err) {
+//     throw new Error("Error connecting to database:", err.message);
+//   }
+//   console.log("Connected to the database");
+// });
 
-module.exports = db;
+// module.exports = db;
